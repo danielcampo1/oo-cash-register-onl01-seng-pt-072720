f5d0dc0e6d13a8128f871dcdc7f552af0ebf1114
daniel campo <campo_daniel94@yahoo.com>
2020-08-21 20:00:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
     self.last_item = price
     self.items.concat([title] * quantity)
     self.total += price * quantity
-    transaction << price * quantity
+    transaction << price
   end
   
   def apply_discount
